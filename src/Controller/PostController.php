aab69822f49e547606af53b2e325c0982e00ad9a
Bryan <bryan.lassalle.pro@gmail.com>
2021-11-26 11:29:59 +0100

    New post form

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Sodium\add;
 
 class PostController extends AbstractController
 {
+
+    const MAX_POSTS_PER_PAGE = 10;
     /**
      * @Route("/")
      */
@@ -31,6 +35,24 @@ class PostController extends AbstractController
 
         return $this->render('post/show.html.twig', [
             'post' => $post,
+        ]);
+    }
+
+    /**
+     * @Route("/new")
+     */
+    public function create(): Response
+    {
+        $post = new Post();
+        $form = $this->createFormBuilder($post)
+            ->add('title')
+            ->add('body')
+            ->add('createdAt')
+            ->getForm()
+            ;
+
+        return $this->renderForm('post/create.html.twig', [
+            'create_form' => $form,
         ]);
     }
 }
